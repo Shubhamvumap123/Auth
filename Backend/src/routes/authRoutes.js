850dd59a7ceeb3276c85a6a84ed63b6a6646ab1d
Shubham Umap <shubhamvumap@gmail.com>
2025-07-15 22:54:45 +0530

    add routes and tokens files

---
+++ Backend/src/routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.post('/login', authController.login);
+router.post('/register', authController.register);
+router.get('/me', authController.getProfile);
+router.put('/me', authMiddleware, authController.updateProfile);
+
+module.exports = router;
